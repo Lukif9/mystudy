1bbae53b466f02e33e123ca8e1c9a16fefa37900
Lukif9 <89239286+Lukif9@users.noreply.github.com>
2021-10-06 23:49:50 +0900

    Add files via upload

---
+++ img.js
@@ -0,0 +1,15 @@
+﻿var express = require('express');
+var myApp = express();
+myApp.use(express.static('myFiles'));
+myApp.get('/', function(req, res){
+    res.send('Hello home page');;
+});
+myApp.get('/images', function(req, res){
+    res.send('My Image, <img src="/1.jpg">')
+})
+myApp.get('/login', function(req, res){
+    res.send('1.html'); // 여긴 어떻게??????
+});
+myApp.listen(2000, function(){
+    console.log('Conneted 2000 port!');
+});
